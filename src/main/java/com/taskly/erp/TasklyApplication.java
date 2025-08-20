0aec27aa84ad17435918e20abea971de6ac27dcd
Trinshika Yadav <trinshika@gmail.com>
2025-08-21 00:57:49 +0530

    first commit

---
+++ src/main/java/com/taskly/erp/TasklyApplication.java
@@ -0,0 +1,13 @@
+package com.taskly.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskly/erp/TasklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskly.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
